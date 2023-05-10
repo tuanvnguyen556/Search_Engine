7e7bbb8d240c7f471b2c359043ffada4033f15c6
j-peros <jperos@uci.edu>
2023-05-09 18:53:30 -0700

    This reads all files in a directory and its subdirectories

---
+++ read_files.py
@@ -0,0 +1,16 @@
+import os
+
+def read_files(filename) -> None:
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"{filename} does not exist as a relative nor absolute path.")
+        # raise an error if this file doesn't exist
+    
+    for file in os.listdir(filename):
+        full_relative = os.path.join(filename, file)
+
+        if os.path.isdir(full_relative): # must iterate through a subdirectory
+            read_files(full_relative)
+        else: # if not a folder, must be a file
+            # some pipeline function to process the file
+            # process(filename)
+            pass
