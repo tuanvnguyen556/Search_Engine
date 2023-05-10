9eb7dd4b94c451b468427b0dc34171ec75ac9998
Kai Wen Ma <kaiwm@uci.edu>
2023-05-10 13:47:21 -0700

    Added tokenize

--- process.py
+++ process.py
@@ -1,12 +1,15 @@
 import json
 import re
 from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
 
 def file_processor(given_file):
     """
     This function processes the json file and retrieves the html content.
     Then, the html content is processed.
     """
+    tokens = []
+    
     open_file = open(given_file, errors='ignore') #opens the file
     try:
         data = json.load(open_file) #loads the json format
@@ -14,9 +17,10 @@ def file_processor(given_file):
         soup = BeautifulSoup(data['content'], 'html.parser') #parses html
         text = soup.get_text(strip=True) #retrieves the content
         textWithoutSymbols = re.sub(r"[^A-Za-z0-9\s]+", "", text) #does some stripping of characters
-        print(textWithoutSymbols)
+        tokens = word_tokenize(textithoutSymbols)
     except:
         pass
+    return tokens
     open_file.close()
 
 
