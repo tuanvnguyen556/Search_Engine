47ea6f3d6d4c3fc5e3383c830942fbb62f6183b9
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-05-10 10:43:43 -0700

    separated the scripts

--- launch.py
+++ launch.py
@@ -1,24 +1,6 @@
-import json
-import re
-from bs4 import BeautifulSoup
-
-
-def load_json(given_file):
-    open_file = open(given_file, errors='ignore')
-    try:
-        data = json.load(open_file)
-        
-        soup = BeautifulSoup(data['content'], 'html.parser')
-        text = soup.get_text(strip=True)
-        textWithoutSymbols = re.sub(r"[^A-Za-z0-9\s]+", "", text)
-        print(textWithoutSymbols)
-    except:
-        pass
-    open_file.close()
 
 def run():
-    given_file = "C:\\8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json"
-    load_json(given_file)
+    pass
 
 if __name__ == "__main__":
     run()
---
+++ process.py
@@ -0,0 +1,25 @@
+import json
+import re
+from bs4 import BeautifulSoup
+
+def file_processor(given_file):
+    """
+    This function processes the json file and retrieves the html content.
+    Then, the html content is processed.
+    """
+    open_file = open(given_file, errors='ignore') #opens the file
+    try:
+        data = json.load(open_file) #loads the json format
+        
+        soup = BeautifulSoup(data['content'], 'html.parser') #parses html
+        text = soup.get_text(strip=True) #retrieves the content
+        textWithoutSymbols = re.sub(r"[^A-Za-z0-9\s]+", "", text) #does some stripping of characters
+        print(textWithoutSymbols)
+    except:
+        pass
+    open_file.close()
+
+
+if __name__ == "__main__":
+    given_file = "C:\\8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json"
+    file_processor(given_file)
