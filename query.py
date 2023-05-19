b40114af5e0407e408f0bb1f0451028f396ce767
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-05-18 18:39:33 -0700

    local branch

--- query.py
+++ query.py
@@ -3,17 +3,28 @@
 import sys
 import inverted_index
 import json
-
+from booleanRetrieval import booleanRetrieval
 def main() -> None:
-    with open("indexer.txt") as f:
-        inverted_index = json.load(f)
+    with open("indexer.txt") as f1:
+        inverted_index = json.load(f1)
+    
+    with open("posting.txt") as f2:
+        the_posting = json.load(f2)
+        
     while True:
-        query = input("Enter a query:")
+        query = input("Enter a query: ").lower()
         if not query:
             print("Please enter a query.")
-            continue
-        query = query.lower() # lowercase query
+        elif query == "quit the query":
+            break
+        else:
+            retrieve_doc = booleanRetrieval()
+            print(retrieve_doc.booleanAndRetrieval(query))
+            
+
+    
         
+
 
     # lst_doc_IDs = some_intersection_function(*[inverted_index.InvertedIndex[term] for term in query])
     # printer(doc_IDs)
