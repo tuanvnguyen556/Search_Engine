3934d01b6b58d484bc68e44186f8b68ab043707b
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-05-20 17:40:38 -0700

    remove printing errors

--- process.py
+++ process.py
@@ -25,7 +25,7 @@ def file_processor(given_file):
             textWithoutSymbols = re.sub(r"[^A-Za-z0-9\s]+", "", text) #does some stripping of characters
             tokens = word_tokenize(textWithoutSymbols.lower()) #tokenizes the string and lowercases them
     except Exception as e:
-        print(e)
+        pass #ignore the error
     open_file.close()
 
     return tokens, data
