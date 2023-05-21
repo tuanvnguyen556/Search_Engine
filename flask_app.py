1c763d7ae0f66fc813e1307169306c4fbe39235f
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-05-20 22:42:34 -0700

    need to merge query code and interface

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__) #create app instance
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"]) #home page
 def home():
-    return "Welcome!"
+    if request.method == "GET":
+        return render_template("app_index.html") #returns the home page
+    else:
+        given_query = request.form["query"]
+        return render_template("redirect_index.html", the_query=given_query) #displays top websites
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) #automatically reruns web server
 
