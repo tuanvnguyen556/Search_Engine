47d48efd79bcb31512e98c83fb6cc2fda4d79507
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-05-29 15:44:19 -0700

    unsorted tsv

--- indexer.py
+++ indexer.py
@@ -4,7 +4,7 @@ import nltk
 from report import make_report
 import json
 import csv
-from inverted_index import InvertedIndex
+
 def index():
     nltk.download('punkt')
     directories = "C://developer"
@@ -26,15 +26,16 @@ def jsonfied_posting(posting: dict):
         json_file2.close()
 
 def tsvfied(indexer: dict):
-    keysToSort = list(indexer.keys())
-    keysToSort.sort()
-
+    #keysToSort = list(indexer.keys())
+    
+    #keysToSort.sort()
+    #print(keysToSort)
     with open("indexer.tsv", "w") as tsv_file:
         tsv_writer = csv.writer(tsv_file, delimiter='\t', lineterminator='\n')
         tsv_writer.writerow(["Token", "Frequency", "PositionsMap"])
-        for k in keysToSort:
+        for k in indexer.keys():
             tsv_writer.writerow([k, indexer[k][0], indexer[k][1:]])
 
 
 if __name__ == "__main__":
-   tsvfied(InvertedIndex.InvertedIndexDict)
+   pass
--- launch.py
+++ launch.py
@@ -1,10 +1,13 @@
-from indexer import index,jsonfied, jsonfied_posting
+from indexer import index,jsonfied, jsonfied_posting, tsvfied
 from inverted_index import InvertedIndex
 from posting_dictionary import Posting_Dict
 import json
 
 
 if __name__ == "__main__":
-    index()
-    jsonfied(InvertedIndex.InvertedIndexDict)
-    jsonfied_posting(Posting_Dict.ID_Posting)
+    #index()
+    with open("indexer.txt") as f1:
+        InvertedIndex.InvertedIndexDict = json.load(f1)
+    
+    tsvfied(InvertedIndex.InvertedIndexDict)
+    #jsonfied_posting(Posting_Dict.ID_Posting)
