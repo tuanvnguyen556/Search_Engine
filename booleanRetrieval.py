2f36c5a96d27d9644fb57fb357d504a61551f1ac
Yash Balaji Pathak <pathaky@circinus-31.ics.uci.edu>
2023-05-29 17:25:14 -0700

    Optimized booleanRetrieval and listIntersection

--- booleanRetrieval.py
+++ booleanRetrieval.py
@@ -4,41 +4,49 @@ class booleanRetrieval:
     """
     This class operates different booleanRetrieval models from the indexer created in milestone 1.
     """
-    def __init__(self, query: str):
-        self.queryList = query.split()
+    def __init__(self):
+        # self.queryList = query.split()
         self.top_urls = []
-    # The booleanAndRetrieval finds the AND intersection between the provided queries.
-    def booleanAndRetrieval(self):        
-        for i in self.queryList:
-            if i not in InvertedIndex.InvertedIndexDict.keys():
-                return []
-        if len(self.queryList) == 1:
-            return InvertedIndex.InvertedIndexDict[self.queryList[0]][1]
-        
-        # Initialize a commonList which contains the common elements from the list of docIDs
-        # corresponding to the given queries
-        queryList = sorted([InvertedIndex.Dict[key] for key in self.queryList], key=len)
-        commonList = self.listIntersection(queryList[0], queryList[1])
-        # commonList = self.listIntersection(InvertedIndex.InvertedIndexDict[queryList[0]][1], InvertedIndex.InvertedIndexDict[queryList[1]][1])
-        
-        # If the query only asks for the intersection of two words, then return the current commonList
-        if len(self.queryList) == 2:
-            return commonList
-        
-        # Otherwise, find the commonList with every other list corresponding to every other
-        # query other than the first 2 queries in the queryList.
-        for index in range(2, len(queryList)):
-            #commonList = self.listIntersection(InvertedIndex.InvertedIndexDict[queryList[index]][1], commonList)
-            commonList = self.listIntersection(queryList[index], commonList)
+    
+    
+    # A helper function which finds the intersection between two given lists
+    def listIntersection(self, givenList: list, commonList: list):
+        # givenList = [[1, 1, 3], [2, 1, 2], [3, 1, 2, 3]]
+        # commonList = [[1, 1, 2], [2, 1, 3]]
+        # Should return: [1, 2]
+        docIDLst1 = [lst[0] for lst in givenList]
+        docIDLst2 = [lst[0] for lst in commonList]
+        return [item for item in docIDLst1 if item in docIDLst2]
 
+
+    # The booleanAndRetrieval finds the AND intersection between the provided queries.
+    def booleanAndRetrieval(self, query: str, *docIDLists: list):
+        # Check if the query is empty
+        if len(docIDLists) == 0:
+            return []
+        # Check if the query is in the dictionary
+        if len(query.split()) != len(docIDLists): 
+            return []
+        
+        # Initialize a commonList which contains the docIDs of the first token
+        commonList = []
+        for lst in docIDLists[0]:
+            commonList.append(lst[0])
+            
+        # If there is only one query, retyurn the list of docIDs
+        # docIDLists = [[[1, 1, 3], [2, 1, 2], [3, 1, 2, 3]]]
+        if len(docIDLists) == 1: return commonList
+        
+        # Otherwise, run the intersection algorithm for the rest of the lists within the docIDLists
+        # docIDLists = [[[1, 1, 3], [2, 1, 2], [3, 1, 2, 3]], [[1, 1, 2], [2, 1, 3]]]
+        for lst in docIDLists[1:]:
+            commonList = self.listIntersection(lst, commonList)
+            
         # Return the final list of common documents
         return commonList
             
     def return_query(self):
         return self.queryList
-    # A helper function which finds the intersection between two given lists
-    def listIntersection(self, givenList: list, commonList: list):
-        return [item for item in givenList if item in commonList]
     
     def print_urls(self, commonList: list):
         if len(commonList) == 0:
