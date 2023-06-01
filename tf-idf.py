2b70ebc6b5709abb6a0fc47d38860f1d62770d95
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-06-01 13:28:18 -0700

    needed to commit

--- tf-idf.py
+++ tf-idf.py
@@ -1,12 +1,15 @@
 from inverted_index import InvertedIndex
 from math import log10
+import json
+
 class Calculatetfidf:
     """This function calaculates the term frequency and inverted document frequency for each term"""
     idf = dict() #stores the idf of each term
+    tf_idf = dict()
     #of the format: Token (key) : tf-idf (value)
 
     @classmethod
-    def calculate_idf(cls, filename: str):
+    def calculate_idf(cls):
         """This function will accept a tsv txt file that contains the inverted index
         content and recieve each token. With every recieved token, this function will
         calculate the tf-idf value for each token given the other information that is next to
@@ -18,5 +21,26 @@ class Calculatetfidf:
         tf = number of time that t occurs in d -> tsv file 
         df = number of documents that contain t -> frequency in InvertedIndex
         """
-        with open(filename) as tsv:
-            pass
+    
+        with open("indexer.txt") as indexer:
+            InvertedIndex.InvertedIndexDict = json.load(indexer)
+    
+    @classmethod
+    def calculate_tf(cls):
+        """ All pseudo code, please change later """
+        N = InvertedIndex.docID
+        
+        for k in InvertedIndex.keys():
+            # Structure of Inv. Index: {Token: [frequency, [docID, pos1, pos2, ...], [docID2, pos1, pos2, ...], ...]}
+            tf_list = [] # TF listing for the current token
+            for t in InvertedIndex[k][1:]:
+                tf_list.append(len(t) - 1) # the TF of each term is the (len-1) from removing the first docID
+
+            df = int() # TODO
+    
+
+           
+
+if __name__ == "__main__":
+    save = Calculatetfidf()
+    save.calculate_idf()
